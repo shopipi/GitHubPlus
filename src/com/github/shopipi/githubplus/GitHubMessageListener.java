5822c9cf6822846c2ffd2d3d8cbdb68d3c166f4c
しょ <r02.gm.2100219@da.iwasaki.ac.jp>
2021-04-22 21:06:39 +0900

    10個以上のファイル更新に対応した

--- src/com/github/shopipi/githubplus/GitHubMessageListener.java
+++ src/com/github/shopipi/githubplus/GitHubMessageListener.java
@@ -37,40 +37,25 @@ public class GitHubMessageListener
 
 			if (!embed.getTitle().toLowerCase().contains(Main.repo.toLowerCase())) return;
 
+			GitHubAPI api = new GitHubAPI(Main.personalAccessToken, Main.owner, Main.repo, Main.branch);
+
 			for (int i = 0; i < descs.length; i++)
 			{
-				// ---------------------------------------------
-				// Get Summary Message
-				String desc   = descs[i];
-				int start     = desc.indexOf(" ") + 1;
-				desc          = desc.substring(start);
-				String author = desc.split(" - ")[1];
-				int end       = desc.lastIndexOf("-") - 1;
-				desc          = desc.substring(0, end);
-				// ---------------------------------------------
+				String desc     = api.getValues("message")[0];
+				String author   = api.getValues("name")[0];
+				String url      = api.getValues("html_url")[0];
+				String commitID = api.getValues("sha")[0].substring(0, 8);
 
-				// ---------------------------------------------
-				// Get URL String
-				String url = descs[i];
-				int start1 = url.indexOf("(") + 1;
-				int end1   = url.indexOf(")");
-				url        = url.substring(start1, end1);
-				// ---------------------------------------------
 
-				// ---------------------------------------------
-				// Get Commit ID
-				String commitID = descs[i];
-				int start2      = commitID.indexOf("[") + 1;
-				int end2        = commitID.indexOf("]");
-				commitID        = commitID.substring(start2, end2);
-				// ---------------------------------------------
-
+				// Get Developer from Commit Author
 				Developer dev = Developer.getDevByGitHubName(author);
 
-				if (dev == null) return;
+				if (dev == null)
+				{
+					dev = new Developer(0, "N/A");
+				}
 
-				GitHubAPI api = new GitHubAPI(Main.personalAccessToken, Main.owner, Main.repo, Main.branch);
-
+				// The Commit is on Target Repo?
 				if (!embed.getTitle().contains(api.repo)) return;
 
 				StringBuilder builder = new StringBuilder();
@@ -92,18 +77,94 @@ public class GitHubMessageListener
 				if (fileCount == 3) color = new Color(0xFFAA00);
 				if (fileCount >= 4) color = new Color(0xFF5555);
 
-				Main.print("File Count: " + fileCount);
+				// ---------------------------------------------
+				// Filecount more than 10
+				// ---------------------------------------------
+				if (fileCount > 10)
+				{
+					// ---------------------------------------------
+					// Title and Commit Message EMBED
+					EmbedBuilder embed01 = new EmbedBuilder()
+							.withTitle(Language.getMsg(Message.ファイルが更新されました))
+							.appendDesc("```" + desc + "```\n")
+							.withColor(color);
 
-				EmbedBuilder embedBuilder = new EmbedBuilder()
-						.withTitle(Language.getMsg(Message.ファイルが更新されました))
-						.appendDesc("```" + desc + "```\n")
-						.appendDesc(":scroll: **" + Language.getMsg(Message.ファイル) + "**： " + builder.toString().substring(0, builder.toString().length() - 2) + "\n\n")
-						.appendDesc(":link: **"   + Language.getMsg(Message.詳細) + "**： [" + commitID + "](" + url + ")\n\n")
-						.appendDesc(":bust_in_silhouette: " + dev.mention() + " - " + author)
-						.withFooterText(fDate)
-						.withColor(color);
+					Main.client.getChannelByID(Main.sendChannelId).sendMessage("----------", embed01.build());
+					// ---------------------------------------------
 
-				Main.client.getChannelByID(Main.sendChannelId).sendMessage(embedBuilder.build());
+					// ---------------------------------------------
+					// Get Filename per 10
+					for (int start_fIndex = 0; start_fIndex < fileCount; start_fIndex += 10)
+					{
+						StringBuilder file10 = new StringBuilder();
+
+						// ---------------------------------------------
+						// Get Filename in per_10 + j index
+						for (int j = start_fIndex; j < start_fIndex + 10; j++)
+						{
+							try
+							{
+								file10.append("[`" + GitHubContents.getFilename(filenames[j])        + "`]");
+								file10.append("(" +  GitHubContents.getContentURL(api, filenames[j]) + ")");
+								file10.append(", ");
+							}
+							catch (ArrayIndexOutOfBoundsException e2)
+							{
+								break;
+							}
+						}
+						// ---------------------------------------------
+
+						// ---------------------------------------------
+						// File names EMBED
+						EmbedBuilder embed02 = new EmbedBuilder();
+
+						if (start_fIndex == 0)
+						{
+							embed02.appendDesc(":scroll: **" + Language.getMsg(Message.ファイル) + "**： ");
+						}
+
+						embed02.appendDesc(file10.toString().substring(0, file10.toString().length() - 2) + "\n\n");
+						embed02.withColor(color);
+
+						Main.client.getChannelByID(Main.sendChannelId).sendMessage(embed02.build());
+						// ---------------------------------------------
+
+						try
+						{
+							// Wait 1sec for Discord Rate Limit
+							Thread.sleep(1000);
+						}
+						catch (InterruptedException e1)
+						{
+						}
+					}
+					// ---------------------------------------------
+
+					EmbedBuilder embed03 = new EmbedBuilder()
+							.appendDesc(":link: **"   + Language.getMsg(Message.詳細) + "**： [" + commitID + "](" + url + ")\n\n")
+							.appendDesc(":bust_in_silhouette: " + dev.mention() + " - " + author)
+							.withFooterText(fDate)
+							.withColor(color);
+
+					Main.client.getChannelByID(Main.sendChannelId).sendMessage(embed03.build());
+
+					Main.client.getChannelByID(Main.sendChannelId).sendMessage("----------");
+				}
+				else
+				{
+					// if Filecount <= 10
+					EmbedBuilder embedBuilder = new EmbedBuilder()
+							.withTitle(Language.getMsg(Message.ファイルが更新されました))
+							.appendDesc("```" + desc + "```\n")
+							.appendDesc(":scroll: **" + Language.getMsg(Message.ファイル) + "**： " + builder.toString().substring(0, builder.toString().length() - 2) + "\n\n")
+							.appendDesc(":link: **"   + Language.getMsg(Message.詳細) + "**： [" + commitID + "](" + url + ")\n\n")
+							.appendDesc(":bust_in_silhouette: " + dev.mention() + " - " + author)
+							.withFooterText(fDate)
+							.withColor(color);
+
+					Main.client.getChannelByID(Main.sendChannelId).sendMessage(embedBuilder.build());
+				}
 			}
 		}
 		catch (IndexOutOfBoundsException ie)
